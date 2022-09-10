6d5d09a8137c72d513d88c82be6427fd9584839e
johnassefaeth <106699972+johnassefaeth@users.noreply.github.com>
2022-09-11 01:57:24 +0300

    Add files via upload

---
+++ krar.js
@@ -0,0 +1,10 @@
+for(var i=0;i<document.getElementsByClassName("kr").length;i++){
+	
+	document.getElementsByClassName("kr")[i].addEventListener("click",krarr);
+}
+
+
+
+function krarr(){
+	alert("zmzmzm");
+}
